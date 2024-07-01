595a3384b2852d7db89ca4a21e7492a5289e4ff4
papdawin <papdawin@protonmail.com>
2024-07-01 23:05:55 +0200

    added daisyui

---
+++ src/assets/mainPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Navbar} from "./Navbar/navbar.tsx";
+
+export class MainPage extends React.Component {
+    render() {
+        return (<>
+            <Navbar />
+            </>
+        )
+    }
+}
