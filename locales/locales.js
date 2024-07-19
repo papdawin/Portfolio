a286b7bb1ea36b480693548123aa1875b8475d11
papdawin <papdawin@protonmail.com>
2024-07-19 11:51:16 +0200

    Updates

---
+++ locales/locales.js
@@ -0,0 +1,6 @@
+const locales = [
+    {short_code: 'gb', country_code: 'ENG', local_name: 'English', eng_name: 'English'},
+    {short_code: 'hu', country_code: 'HUN', local_name: 'Magyar' , eng_name: 'Hungarian'},
+    {short_code: 'de', country_code: 'GER', local_name: 'Deutsch', eng_name: 'German'}
+]
+export default locales;
