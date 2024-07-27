8655798dc15eedc2feff18735efc85b0f6302a2d
papdawin <papdawin@protonmail.com>
2024-07-27 13:27:45 +0200

    added image-processing project

--- src/assets/content/Home/Projects/projectLoader.ts
+++ src/assets/content/Home/Projects/projectLoader.ts
@@ -3,12 +3,18 @@ export default [
         id: 1,
         image: "",
         github: "https://github.com/papdawin/Portfolio",
-        link: ""
+        link: "https://pdwn.hu"
     },
     {
         id: 2,
         image: "",
         github: "https://github.com/papdawin/BSc-Thesis",
+        link: ""
+    },
+    {
+        id: 3,
+        image: "",
+        github: "https://github.com/papdawin/image-processing",
         link: ""
     }
 ]
